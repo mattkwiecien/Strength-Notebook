8022d32d3ff760460c66455289110e0b04a75d96
Matthew Kwiecien <mkwieci2@gmail.com>
2018-04-21 12:00:06 -0500

    Initial asp.net core MVC applicatioin commit

---
+++ StrengthTrackerWeb/StrengthTrackerWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StrengthTrackerWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
