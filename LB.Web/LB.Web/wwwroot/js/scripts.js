f2b08dfa1b7043a50d4c2e0409f8f64211dc9df0
Matthew Kwiecien <mkwieci2@gmail.com>
2018-04-28 17:19:12 -0500

    Removing untracked files.

--- LB.Web/LB.Web/wwwroot/js/scripts.js
+++ LB.Web/LB.Web/wwwroot/js/scripts.js
@@ -1,5 +1,5 @@
 var LBApp = angular.module('LBApp', []);
-LBApp.controller('currentWorkoutController', ['$scope', function ($scope) {
+LBApp.controller('currentWorkoutController', ['$scope', '$http', function ($scope, $http) {
     $scope.sets;
     $scope.exerciseRows = [];
 
@@ -19,6 +19,10 @@ LBApp.controller('currentWorkoutController', ['$scope', function ($scope) {
         $scope.exerciseRows.push(new ExerciseRow());
     }
 
+    $scope.save = function () {
+        
+    }
+
     function createRangeArrayFromNumber(number) {
         var arr = [];
         for (var i = 0; i < number; i++) {
@@ -30,4 +34,42 @@ LBApp.controller('currentWorkoutController', ['$scope', function ($scope) {
     function ExerciseRow() {
         return { isEmpty: true }
     }
+}]);
+LBApp.controller('dashboardController', ['$scope', function ($scope) {
+    var myLineChart = new Chart("liftChart", {
+        type: 'line',
+        data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
 }]);
