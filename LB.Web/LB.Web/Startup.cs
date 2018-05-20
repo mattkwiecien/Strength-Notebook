349473518615ee33d6f928586e30562ee79b70c7
Matthew Kwiecien <mkwieci2@gmail.com>
2018-05-20 09:42:20 -0500

    Adding ASP.NET Core individual user accounts.

--- LB.Web/LB.Web/Startup.cs
+++ LB.Web/LB.Web/Startup.cs
@@ -8,6 +8,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
+using LB.Web.Data;
+using Microsoft.EntityFrameworkCore;
+using LB.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using LB.Web.Services;
 
 namespace LB.Web {
     public class Startup {
@@ -19,6 +24,17 @@ namespace LB.Web {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, EmailSender>();
+
             services.AddMvc();
         }
 
@@ -27,6 +43,7 @@ namespace LB.Web {
             if (env.IsDevelopment()) {
                 app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
                 //Add reference to local js files in development
                 app.UseFileServer(new FileServerOptions() {
                     FileProvider = new PhysicalFileProvider(Path.Combine(env.ContentRootPath, "Content")),
@@ -37,7 +54,8 @@ namespace LB.Web {
             }
 
             app.UseStaticFiles();
-            
+            app.UseAuthentication();
+
             app.UseMvc(routes => {
                 routes.MapRoute(
                     name: "default",
